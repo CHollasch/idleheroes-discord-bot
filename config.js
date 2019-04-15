4fd28bfb729d734cd8652bab62ac570ad5b18c35
Connor Hollasch <chollasch@gmail.com>
2019-04-14 19:32:41 -0700

    Unignore config

---
+++ config.js
@@ -0,0 +1,7 @@
+process.env.TOKEN = 'NTU4NDM1NjA5NzkwNjQ0MjM0.D3XN5w.ebvN92LRLsxSIZKUt9gXzCFBx60';
+
+module.exports = {
+    commandPrefixes: [
+        'ih', 'idleheroes', 'ihb', 'idlebot', 'idle'
+    ]
+};
